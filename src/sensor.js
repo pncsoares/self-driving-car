01935144195643d83c605bb0bad2879873a92fe4
Pedro Soares <pncsoares@gmail.com>
2022-05-18 08:00:26 +0100

    Replace === with ==

--- src/sensor.js
+++ src/sensor.js
@@ -30,7 +30,7 @@ class Sensor {
                 lerp(
                     this.raySpread / 2,
                     -this.raySpread / 2,
-                    this.rayCount === 1 ? 0.5 : i / (this.rayCount - 1)
+                    this.rayCount == 1 ? 0.5 : i / (this.rayCount - 1)
                 ) + this.car.angle;
 
             const start = { x: this.car.x, y: this.car.y };
@@ -75,12 +75,12 @@ class Sensor {
             }
         }
 
-        if (touches.length === 0) {
+        if (touches.length == 0) {
             return null;
         } else {
             const offsets = touches.map((e) => e.offset);
             const minOffset = Math.min(...offsets);
-            return touches.find((e) => e.offset === minOffset);
+            return touches.find((e) => e.offset == minOffset);
         }
     }
 
